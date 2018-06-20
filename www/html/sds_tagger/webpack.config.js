31df0a26d0288d77a43b0abcb8c2fbfcae41d40e
Maswor <maswortxt@gmail.com>
2018-06-20 07:25:20 -0500

    refactoring touch event

---
+++ www/html/sds_tagger/webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+	mode: 'production',
+	entry: './app.ts',
+	output: {
+		filename: 'bundle.js',
+		path: `${__dirname}/build`,
+		publicPath: '/build/',
+	},
+	watchOptions: {
+		ignored: ['/**/*.js', 'node_modules']
+	},
+
+	// Enable sourcemaps for debugging webpack's output.
+	devtool: 'source-map',
+
+	resolve: {
+		// Add '.ts' and '.tsx' as resolvable extensions.
+		extensions: ['.ts', '.js', '.json'],
+	},
+
+	module: {
+		rules: [{
+			test: /\.ts?$/,
+			use: 'ts-loader',
+			exclude: /node_modules/
+		}, ],
+	},
+
+	devServer: {
+		host: '0.0.0.0',
+		port: 8080,
+		open: true,
+	},
+
+};
